5240a0caef70d80c5e072994a9d6af17e5c7efc5
Dmitry Krutikov <d.krutikov@gmail.com>
2022-10-21 01:10:48 +0300

    Typo fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,27 +2,22 @@
 
 set -e
 
-mkdir -p -m 0750 /run/php
-
 if [ "$(id -u)" = "0" ]; then
-  if [ -n "$UID" ] && [ ! "$UID" = "$(id php -u)" ]; then
-    usermod -u "$UID" php
+  if [ -n "$UID" ] && [ ! "$UID" = "$(id node -u)" ]; then
+    usermod -u "$UID" node
   fi
 
-  if [ -n "$GID" ] && [ ! "$GID" = "$(id php -g)" ]; then
-    groupmod -g "$GID" php
+  if [ -n "$GID" ] && [ ! "$GID" = "$(id node -g)" ]; then
+    groupmod -g "$GID" node
   fi
 
-  chown -R php:php \
-    /run/php \
-    /var/lib/php \
-    /var/log/php
+  chown -R node:node /var/lib/node \
 
   if [ -d "/lib/entrypoint" ]; then
     run-parts -v --regex '.*sh$' /lib/entrypoint
   fi
 
-  exec gosu php "$@"
+  exec gosu node "$@"
 else
   exec "$@"
 fi
